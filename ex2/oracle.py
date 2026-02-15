ce65d485779505da85193c726f46932f01cc3079
Danylo Borysenko <borysenkodanyl@gmail.com>
2026-02-15 14:11:58 +0100

    ex2 was finished

--- ex2/oracle.py
+++ ex2/oracle.py
@@ -4,16 +4,71 @@ import os
 def dependencies_check() -> bool:
     try:
         from dotenv import load_dotenv
+        load_dotenv()
         return True
     except ModuleNotFoundError:
         print("dotenv module is not found. Use 'pip install python-dotenv'")
         return False
 
 
+def configuration_check() -> bool:
+    env_var = None
+    try:
+        with open(".env.example") as f:
+            env_var = f.readlines()
+    except FileNotFoundError:
+        print("File '.env.example' does not exist")
+        return False
+    for var in env_var:
+        var_name = var.strip().split("=")[0]
+        value = os.getenv(var_name)
+        if not value:
+            print(f"Missing value for environment variable '{var_name}'")
+            return False
+    return True
+
+
+def security_check() -> None:
+    print("Environment security check:")
+    print("[OK] No hardcoded secrets detected")
+    print("[OK] Production overrides available")
+    try:
+        with open(".gitignore") as f:
+            vars = f.readlines()
+            vars = [var.strip() for var in vars]
+            if ".env" in vars:
+                print("[OK] .env added to .gitignore")
+            else:
+                print("[Error] .env was not added to .gitignore")
+    except FileNotFoundError:
+        print("[WARNING] .gitignore file missing")
+# [OK] No hardcoded secrets detected
+
+
 if __name__ == "__main__":
     if not dependencies_check():
         exit(1)
-    from dotenv import load_dotenv
-    load_dotenv()
-    db_url = os.environ["DATABASE_URL"]
-    print(db_url)
+    if not configuration_check():
+        exit(1)
+    mode = os.getenv("MATRIX_MODE", "development")
+    if mode == "production":
+        print("\n⚠ Production mode detected\n")
+
+    print("ORACLE STATUS: Reading the Matrix...")
+    print("\nConfiguration loaded:")
+    print(f"Mode: {mode}")
+    if mode == "development":
+        print("Database: Connected to local instance")
+    else:
+        print("Database: Connected to production cluster")
+    print("API Access: Authenticated")
+    masked_key = ""
+    for _ in range(0, len(os.getenv('API_KEY'))):
+        masked_key = masked_key + "*"
+    print(f"Api_key: {os.getenv("API_KEY")
+                      if mode == "development" else masked_key}")
+    print(f"Log Level: {os.getenv('LOG_LEVEL')}")
+    print("Zion Network: Online")
+    print()
+    security_check()
+    print("\nThe Oracle sees all configurations.")
